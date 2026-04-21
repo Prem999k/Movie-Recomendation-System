c62065dd8aa67a0f68baa9f1073cd6cbfc1b9ad2
Prem Kumar K <166680374+Prem999k@users.noreply.github.com>
2026-04-21 23:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings("ignore")
 
 # =============================
 # LOAD DATA
